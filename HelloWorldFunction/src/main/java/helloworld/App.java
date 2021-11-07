69cc6198327ff82c3d117b3cf6649a2714fff9bd
Thanthu <tnair@orthofx.com>
2021-11-07 09:42:05 +0530

    Create a lambda function that accepts and returns a List. Closes #6.

--- HelloWorldFunction/src/main/java/helloworld/App.java
+++ HelloWorldFunction/src/main/java/helloworld/App.java
@@ -1,5 +1,12 @@
 package helloworld;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+// This is for demo purposes. In real application keep only one Lambda function and private methods it uses in one class.
+// Do not write more than one Lambda function in one class.
 public class App {
 
 	public String hello(String name) {
@@ -8,6 +15,20 @@ public class App {
 	
 	public int getNumber(float num) {
 		return (int) num;
+	}
+	
+	public List<Integer> getScores(List<String> names) {
+		Map<String, Integer> studentScores = new HashMap<>();
+		studentScores.put("Luffy", 90);
+		studentScores.put("Naruto", 89);
+		studentScores.put("Jiraya", 95);
+		
+		List<Integer> results = new ArrayList<>();
+		names.forEach(name -> {
+			results.add(studentScores.get(name));
+		});
+		
+		return results;
 	}
 	
 }
