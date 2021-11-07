d04c7283bdb03daa77f44b95ad415522e40f5bd5
Thanthu <tnair@orthofx.com>
2021-11-07 20:38:59 +0530

    Create a lambda function that accepts InputStream and writes to OutputStream. Closes #9.

--- HelloWorldFunction/src/main/java/helloworld/App.java
+++ HelloWorldFunction/src/main/java/helloworld/App.java
@@ -1,5 +1,8 @@
 package helloworld;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -41,6 +44,13 @@ public class App {
 	public ClinicalData getClinicalData(Patient patient) {
 		System.out.println(patient);
 		return new ClinicalData("Normal", "Normal");
+	}
+
+	public void getOutput(InputStream in, OutputStream out) throws IOException {
+		int data;
+		while ((data = in.read()) != -1) {
+			out.write(Character.toLowerCase(data));
+		}
 	}
 
 }
